aa64454f0e9ca72d2e4cfcbd61bd21df43fa3edf
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-09-28 21:03:17 +0400

    UI Development

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,15 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:video_and_voice_call_app_using_zegocloud/constant/constants.dart';
+import 'package:video_and_voice_call_app_using_zegocloud/service/login_service.dart';
+import 'package:video_and_voice_call_app_using_zegocloud/utils/util.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<StatefulWidget> createState() => LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class LoginPageState extends State<LoginPage> {
+  final _userIDTextCtrl = TextEditingController(text: 'user_id');
+  final _passwordVisible = ValueNotifier<bool>(false);
+
+  @override
+  void initState() {
+    super.initState();
+
+    getUniqueUserId().then((userID) async {
+      setState(() {
+        _userIDTextCtrl.text = userID;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            logo(),
+            const SizedBox(height: 50),
+            userIDEditor(),
+            passwordEditor(),
+            const SizedBox(height: 30),
+            signInButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget logo() {
+    return Center(
+      child: Image.asset(
+        'assets/image.png',
+        width: 200,
+        height: 200,
+      ),
+    );
+  }
+
+  Widget userIDEditor() {
+    return TextFormField(
+      controller: _userIDTextCtrl,
+      decoration: const InputDecoration(
+        labelText: 'Phone Num.(User for user id)',
+      ),
+    );
+  }
+
+  Widget passwordEditor() {
+    return ValueListenableBuilder<bool>(
+      valueListenable: _passwordVisible,
+      builder: (context, isPasswordVisible, _) {
+        return TextFormField(
+          obscureText: !isPasswordVisible,
+          decoration: InputDecoration(
+            labelText: 'Password.(Any character for test)',
+            suffixIcon: IconButton(
+              icon: Icon(
+                isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+              ),
+              onPressed: () {
+                _passwordVisible.value = !_passwordVisible.value;
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget signInButton() {
+    return ElevatedButton(
+      onPressed: _userIDTextCtrl.text.isEmpty
+          ? null
+          : () async {
+              login(
+                userId: _userIDTextCtrl.text,
+                userName: 'user_${_userIDTextCtrl.text}',
+              ).then((value) {
+                onUserLogin();
+
+                Navigator.pushNamed(
+                  context,
+                  PageRouteName.home,
+                );
+              });
+            },
+      child: const Text('Sign In', style: textStyle),
+    );
   }
 }
